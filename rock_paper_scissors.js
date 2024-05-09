9ddf694bbd671743b12f4fccfbe2481f251f371d
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-05-09 12:39:30 -0400

    Add project plan

---
+++ rock_paper_scissors.js
@@ -0,0 +1,10 @@
+// Generate random choice for the computer
+// Get choice from user
+// Create variables to track score
+// Create a game round
+    // compare the choice of user and computer
+    // tally score based on the result
+// Repeat round 5 times
+    // an entire game is 5 rounds
+    // for each round continue to add to the score
+    // whoever has the highest score at the end of the 5 rounds is declared the winner
