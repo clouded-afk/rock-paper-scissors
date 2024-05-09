8304e044805e6e4216e2189af72a823459bdb48e
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-05-09 14:40:24 -0400

    Edit userChoice function -- Add logic for a single round

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -14,25 +14,58 @@
 
 // Generates a random number between 0-2, the number is variable is then assigned to rock, paper, or scissors based on the random number generated
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3)
+    let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
         computerChoice = "rock";
     } else if (computerChoice === 1) {
         computerChoice = "paper";
     } else if (computerChoice === 2) {
-        computerChoice = "scissors"
+        computerChoice = "scissors";
     }
-    return computerChoice
+    return computerChoice;
 }
 
 
 // Prompts user to enter rock, paper, or scissors. Accounts for 3 different ways of entering the option and converts the user choice to all lowercase
-function getHumanChoice() {
-    let humanChoice = prompt("Enter rock, paper, or scissors!")
-    return humanChoice
+function getUserChoice() {
+    let userChoice = prompt("Enter rock, paper, or scissors!").toLowerCase();
+    while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+        userChoice = prompt("Invalid choice. please enter rock, paper, or scissors!").toLowerCase();
+    }
+    return userChoice;
 }
 
 // Variables to keep track of user and computer score
-let humanScore = 0
-let computerScore = 0
+let userScore = 0;
+let computerScore = 0;
+
+
+function playRound(userChoice, computerChoice) {
+    if (userSelection.toLowerCase() === userChoice.toLowerCase()) {
+        if (userSelection === "rock" && computerChoice === "paper") {
+            console.log('The computer wins this round! Paper beats Rock!');
+            computerScore += 1;
+        } else if (userSelection === "rock" && computerChoice === "scissors") {
+            console.log('You win this round! Rock beats Scissors!');
+            userScore += 1;
+        } else if (userSelection === "paper" && computerChoice === "scissors") {
+            console.log('The computer wins this round! Scissors beats Paper!');
+            computerScore += 1;
+        } else if (userSelection === "paper" && computerChoice === "rock") {
+            console.log('You win this round! Paper beats Rock!');
+            userScore += 1;
+        } else if (userSelection === "scissors" && computerChoice === "rock") {
+            console.log('The computer wins this round! Rock beats Scissors!');
+            computerScore += 1;
+        } else if (userSelection === "scissors" && computerChoice === "paper") {
+            console.log('You win this round! Scissors beats Paper!');
+            userScore += 1;
+        } else {
+            console.log("Result of the round is a Draw! Nobody gets a point!");
+        }
+    }
+}
+
+const userSelection = getUserChoice();
+const computerSelection = getComputerChoice();
 
